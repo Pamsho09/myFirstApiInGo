a52670eb5c9a8a36a57a2061e90b61fb41ce3f67
pamsho <francsico@tinkin.one>
2021-09-14 23:23:22 -0500

    Added heroku.yml

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /usr/
 COPY go.mod ./
 RUN go mod download
 COPY ./ .
-EXPOSE 43760
+
 CMD [ "go", "run", "main.go" ]
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	useController "myFirstApiWithGo/user"
 
@@ -14,15 +15,25 @@ type User struct {
 	Numero1 int `json:"numero1"`
 	Numero2 int `json:"numero2"`
 }
+func goDotEnvVariable(key string) string {
 
+	// load .env file
+	err := godotenv.Load(".env")
+  
+	if err != nil {
+	  log.Fatalf("Error loading .env file")
+	}
+  
+	return os.Getenv(key)
+  }
 func main() {
 	godotenv.Load()
 	//se crea la app de fiber
 	app := fiber.New()
 	app.Use(cors.New())
 	//crea una ruta para el home
-	log.Println("Iniciando el servidor en el puerto 43760")
+	log.Println("Iniciando el servidor en el puerto ",goDotEnvVariable("PORT"))
 	useController.UserController(app)
 	//esto hace que corra la app
-	log.Fatal(app.Listen(":43760"))
+	log.Fatal(app.Listen(":"+goDotEnvVariable("PORT")))
 }
